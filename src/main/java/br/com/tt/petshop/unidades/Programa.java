dbb6def372763fc57b2e537a4dd1b19a6fee898e
Juliana Blicharz <78827145+JulyBlicharz@users.noreply.github.com>
2021-05-20 20:56:47 -0300

    Update Programa.java
    Inserir lista de Unidades no Banco

--- src/main/java/br/com/tt/petshop/unidades/Programa.java
+++ src/main/java/br/com/tt/petshop/unidades/Programa.java
@@ -1,19 +1,16 @@
 package br.com.tt.petshop.unidades;
 
-import java.io.File;
 import java.io.IOException;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 
 public class Programa {
+
 
     public static void main(String[] args) throws SQLException, IOException {
         //Criar uma classe Arquivo que le e retorna as petshop's (List<unidades>)
         // Criar uma instania de concexao, no database db_aluno3 (OK)
-        //Fazer um for na listaDeUniadades, inserinf cada unidade na tabela tb_unidade (id, nome, endereco)
+        //Fazer um for na listaDeUniadades, inserir cada unidade na tabela tb_unidade (id, nome, endereco)
 
         Conexao minhaConexao = new Conexao();
         Arquivo arquivo = new Arquivo();
@@ -22,16 +19,30 @@ public class Programa {
              System.out.println(listaDeUniadades.getDescricaoUnidades());
         }
 
+
         String usuario = "admin";
         String senha = "Imers40JavaAdmin";
-        String url = "jdbc:postgresql://165.227.108.225:5432/db_aluno3";
+        String url = "jdbc:postgresql://pg.lptn.com.br:5432/db_aluno3";
         minhaConexao.conectar(url, usuario, senha);
 
-        
-        String sql = "insert into tb_unidade (nome, endereco) values ('?', '?');", // String nome String endereco;
-        minhaConexao.inserir(sql);
 
+         for (Unidade listaDeUniadades: Arquivo.LerUnidadePetshop()){
+             String sql = "insert into tb_unidade (nome, endereco) values (?,?);";
+            try {
+                PreparedStatement preparedStatement = minhaConexao.inserir(sql);
+                preparedStatement.setString(1, listaDeUniadades.getNome());
+                preparedStatement.setString(2, listaDeUniadades.getEndereco());
+                preparedStatement.execute();
+                preparedStatement.close();
+            }catch (SQLException e){
+                throw new SQLException("Erro Sql");
+            }
+             //System.out.println(listaDeUniadades.getDescricaoUnidades());
+        }
 
+        //String sql = "insert into tb_unidade (nome, endereco) values ('?', '?');";
+        // String nome String endereco;
+        //minhaConexao.inserir(sql);
 
     }
 
